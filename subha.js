a7905d460fb4e376e2ac2ce3806665e1784474e5
Susan <shresthasusan44@gmail.com>
2023-06-24 08:56:04 +0545

    -committed

--- subha.js
+++ subha.js
@@ -1,5 +1,5 @@
 
-
+// <======== mobile burger Script =======>
 let burger = document.getElementById('burger'),
 	 nav    = document.getElementById('main-nav'),
     nav_link = document.getElementById('nav-list'),
@@ -17,26 +17,55 @@ burger.addEventListener('click', function(e){
 
 	}
 
-);
+);	
 
 childlist[0].addEventListener('click', function(e){
 	burger.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
+	container[0].style.zIndex = "-1"
 });
 
 childlist[1].addEventListener('click', function(e){
 	burger.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
+	container[0].style.zIndex = "-1"
 });
 
 childlist[2].addEventListener('click', function(e){
 	burger.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
+	container[0].style.zIndex = "-1"
 });
 
 childlist[3].addEventListener('click', function(e){
 	burger.classList.toggle('is-open');
 	nav.classList.toggle('is-open');
+	container[0].style.zIndex = "-1"
 });
 
+// var body = document.getElementsByTagName('body')[0];
+// body.style.backgroundColor = 'green';
 
+// // trigger this function every time the user scrolls
+// window.onscroll = function (event) {
+// 	var scroll = window.pageYOffset;
+// 	if (scroll < 300) {
+// 		// green
+// 		body.style.backgroundColor = 'green';
+// 	} else if (scroll >= 300 && scroll < 600) {
+// 		// yellow
+// 		body.style.backgroundColor = 'yellow';
+// 	} else if (scroll >= 600 && scroll < 1200) {
+// 		// blue
+// 		body.style.backgroundColor = 'blue';
+// 	} else if (scroll >= 1200 && scroll < 1800) {
+// 		// orange
+// 		body.style.backgroundColor = 'orange';
+// 	} else if (scroll >= 1800 && scroll < 3000) {
+// 		// red
+// 		body.style.backgroundColor = 'red';
+// 	} else {
+// 		// purple
+// 		body.style.backgroundColor = 'purple';
+// 	}
+// }
